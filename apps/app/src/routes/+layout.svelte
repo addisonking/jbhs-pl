ed6a2a9d764c1fa68ab5ad7e42b2e868c5d8867d
Addison King <addison@othius.com>
2025-03-17 12:22:12 -0400

    feat(app): add live map button

--- apps/app/src/routes/+layout.svelte
+++ apps/app/src/routes/+layout.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import { Map } from '@lucide/svelte';
+	import { page } from '$app/state';
+
 	import '../app.pcss';
+
 	let { children } = $props();
 </script>
 
 <div class="flex h-screen flex-col justify-between">
+	{#if page.url.pathname !== '/'}
+		<header class="bg-background container z-40">
+			<div class="flex h-16 items-center justify-end">
+				<Button variant="outline" href="/">
+					<Map class="mr-1" /> Live Map
+				</Button>
+			</div>
+		</header>
+	{/if}
+
 	<div class="my-auto">
 		{@render children()}
 	</div>
@@ -11,7 +26,7 @@
 	<footer class="py-6 md:px-8 md:py-0">
 		<div class="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
 			<div class="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-				<p class="text-center text-sm leading-loose text-muted-foreground md:text-left">
+				<p class="text-muted-foreground text-center text-sm leading-loose md:text-left">
 					<a
 						href="https://www.johnbowne.org"
 						target="_blank"
@@ -20,8 +35,8 @@
 					>
 						John Bowne High School</a
 					>
-					Parking Lot Managment System
-					- <a href="/privacy" class="font-medium underline underline-offset-4">Data Usage</a>
+					Parking Lot Managment System -
+					<a href="/privacy" class="font-medium underline underline-offset-4">Data Usage</a>
 				</p>
 			</div>
 		</div>
