b73f517ff9cec60f383d3ca7c5e65ec5400472b8
Addison King <addison@othius.com>
2025-03-24 13:57:30 -0400

    feat(app-eol): add eol notifcation page

---
+++ apps/app-eol/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1>End of Life Notification</h1>
+<p>
+  This application has reached its end of life and is no longer being actively
+  maintained. It will not receive further updates, and the associated physical
+  implementation has been discontinued.
+</p>
+<p>
+  For any questions or concerns, please contact the administrator at
+  box@addisonking.rocks.
+</p>
